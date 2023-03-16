5f1ba6c0c3f65f58b5c7db4b8c8a541c596b660f
Paul Walker <paul@pwjw.com>
2023-03-16 18:06:27 -0400

    Threading lets us actually send messages

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,97 @@
 
 #include "RtMidi.h"
 #include <isocline.h>
+#include <queue>
+#include <string>
+#include <thread>
+#include <chrono>
+
+std::queue<std::string> messages;
+std::mutex messageMutex;
+
+
+void runMidiThread()
+{
+    using namespace std::chrono_literals;
+    auto midiout = std::make_unique<RtMidiOut>();
+    unsigned int nPorts = midiout->getPortCount();
+    std::cout << "\nThere are " << nPorts << " MIDI output ports available. Picking 0\n";
+
+    std::string portName = midiout->getPortName(0);
+    std::cout << "  Sending : " << portName << '\n';
+    midiout->openPort(0);
+
+    while(true)
+    {
+        std::string msg {};
+        int count{0};
+        {
+            std::lock_guard<std::mutex> g(messageMutex);
+            if (!messages.empty())
+            {
+                msg = messages.back();
+                messages.pop();
+                count = messages.size();
+            }
+        }
+
+        if (msg == "quit")
+        {
+            std::cout << "BYEE" << std::endl;
+            return;
+        }
+        if (msg[0] == '>')
+        {
+            auto n = std::atoi(msg.c_str() + 1);
+            unsigned char mm[4];
+            mm[0] = 0x90;
+            mm[1] = n;
+            mm[2] = 90;
+            midiout->sendMessage(mm, 3);
+        }
+        if (msg[0] == '<')
+        {
+            auto n = std::atoi(msg.c_str() + 1);
+            unsigned char mm[4];
+            mm[0] = 0x80;
+            mm[1] = n;
+            mm[2] = 90;
+            midiout->sendMessage(mm, 3);
+        }
+
+        if (!msg.empty())
+            std::cout << "THREAD " << msg << std::endl;
+
+        if (!count)
+            std::this_thread::sleep_for(1ms);
+    }
+}
 
 int main(int, char **)
 {
-  std::cout << "Hello World" <<std::endl;
+    std::thread t(runMidiThread);
 
-  auto midiout = std::make_unique<RtMidiOut>();
-unsigned int nPorts = midiout->getPortCount();
-      std::cout << "\nThere are " << nPorts << " MIDI output ports available.\n";
+    ic_set_history(nullptr, -1 /* default entries (= 200) */);
+    char *input;
+    while ((input = ic_readline("prompt")) != nullptr)
+    {
+        {
+            std::lock_guard<std::mutex> g(messageMutex);
+            messages.push(std::string(input));
+        };
+        if (std::string(input) == "quit" )
+        {
+            t.join();
+            return 0;
+        }
+        free(input);
+    }
 
-      for ( unsigned i=0; i<nPorts; i++ ) {
-        std::string portName = midiout->getPortName(i);
-        std::cout << "  Input Port #" << i << ": " << portName << '\n';
-      }
-ic_set_history(nullptr, -1 /* default entries (= 200) */);
-char* input;
-while( (input = ic_readline("prompt")) != NULL ) { // ctrl+d/c or errors return NULL
-  printf("you typed:\n%s\n", input); // use the input
-  free(input);  
-}
+    {
+        std::lock_guard<std::mutex> g(messageMutex);
+        messages.push("quit");
+    }
 
-  return 0;
+    t.join();
+    return 0;
 }
